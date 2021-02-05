7af5722da0c2c775fa92a02047a4ba905c2fd355
Matthew Ferguson <matthew.ferguson@xtra.co.nz>
2021-02-06 06:20:53 +1300

    Initial Commit

---
+++ StrongholdGen/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StrongholdGen
+//
+//  Created by Matthew Ferguson on 6/02/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
